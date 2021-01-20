1773d3f9c11118911745e8260ea9612b88f4d0b5
12jking <12jkinghb@gmail.com>
2021-01-20 13:34:35 +0100

    Added download and delete function

--- src/main/java/de/j/updater/main/Main.java
+++ src/main/java/de/j/updater/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             Paper paper = new Paper();
-            System.out.println("The newest version is " + paper.getNewestVersion());
+            System.out.println("The newest version is #" + paper.getNewestVersion());
             Server.deleteOldVersion();
             paper.downloadNewVersion();
         } catch (IOException | InterruptedException e) {
