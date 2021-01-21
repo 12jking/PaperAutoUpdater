acacc446a73eebfa21eb8cf226b78ba9e9da29ae
12jking <12jkinghb@gmail.com>
2021-01-21 12:37:29 +0100

    comparing old version with new version

--- src/main/java/de/j/updater/util/Server.java
+++ src/main/java/de/j/updater/util/Server.java
@@ -3,6 +3,7 @@ package de.j.updater.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class Server {
 
@@ -43,7 +44,7 @@ public class Server {
     }
 
     public static boolean alreadyNewestVersion(String mcVersion, int build){
-        return getCurrentVersion().equalsIgnoreCase("paper-" + mcVersion + "-" + Paper.newestVersion + ".jar");
+        return Objects.requireNonNull(getCurrentVersion()).equalsIgnoreCase("paper-" + mcVersion + "-" + Paper.newestVersion + ".jar");
     }
 
 }
